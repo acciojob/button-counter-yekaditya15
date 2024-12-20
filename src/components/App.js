a86cd88b1c9c948f6a806865b34813b4b603d34c
yekaditya11 <yekaditya11@gmail.com>
2024-12-20 12:34:44 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const handelClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times</p>
+      <button onClick={handelClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
